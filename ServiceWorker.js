7527885f1a6e2bb50fe0449a950fdf7484311d63
minthood334 <115146187+minthood334@users.noreply.github.com>
2023-09-23 23:24:54 +0900

    Add files via upload

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "TEAM3-play.io-1.0";
 const contentToCache = [
     "Build/playioweb.loader.js",
-    "Build/playioweb.framework.js.gz",
-    "Build/playioweb.data.gz",
-    "Build/playioweb.wasm.gz",
+    "Build/playioweb.framework.js.unityweb",
+    "Build/playioweb.data.unityweb",
+    "Build/playioweb.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
